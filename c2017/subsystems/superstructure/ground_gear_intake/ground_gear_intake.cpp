69fa9483b3fed677d147542e76077981053bbac2
Apple61752 <mohamedapple@hotmail.com>
2017-01-19 19:23:42 -0800

    Rename is_solenoid_up to intake_down and remove ! from output

--- c2017/subsystems/superstructure/ground_gear_intake/ground_gear_intake.cpp
+++ c2017/subsystems/superstructure/ground_gear_intake/ground_gear_intake.cpp
@@ -5,11 +5,11 @@ namespace c2017 {
 
 namespace ground_gear_intake {
   
-  bool is_solenoid_up_ = false; //will lift when intake stalls
+  bool intake_down_ = true; //will lift when intake stalls
   bool has_current_spiked_ = false;
 
 GroundGearIntake::GroundGearIntake() {
-  is_solenoid_up_ = false; //will lift when intake stalls
+  intake_down_ = true; //will lift when intake stalls
   has_current_spiked_ = false;
 }
 
@@ -19,31 +19,31 @@ GroundGearIntakeOutputProto GroundGearIntake::Update(GroundGearIntakeInputProto 
   
   if(ground_intake_goal == GroundGearIntakeGoal::CARRY) {
     voltage = 0;
-    is_solenoid_up_ = true;
+    intake_down_ = false;
     has_current_spiked_ = false;
   }
   
   if(ground_intake_goal == GroundGearIntakeGoal::PICKUP) {
     voltage = 12;
-    is_solenoid_up_ = false;
+    intake_down_ = true;
     if (input->current() > 120) { //the intake stalls when its current spikes
       has_current_spiked_ = true; 
     }
     if (has_current_spiked_) {
       voltage = 0; //stop moving when the motor stalls
-      is_solenoid_up_ = true;
+      intake_down_ = false;
     }
   }
   
   if(ground_intake_goal == GroundGearIntakeGoal::SCORE) {
     voltage = -12; //outtake
-    is_solenoid_up_ = true;
+    intake_down_ = false;
     has_current_spiked_ = false;
   } //sends voltage and solenoid output
   
   GroundGearIntakeOutputProto output;
   output->set_roller_voltage(voltage);
-  output->set_solenoid(!is_solenoid_up_);
+  output->set_solenoid(intake_down_);
   return output;
 }
 
