740ae01db553a8c78e15b450b2c808262cf61f8c
Apple61752 <mohamedapple@hotmail.com>
2017-01-19 18:49:12 -0800

    correct header guards and remove irrelevant tests

--- c2017/subsystems/superstructure/ground_gear_intake/ground_gear_intake_test.cpp
+++ c2017/subsystems/superstructure/ground_gear_intake/ground_gear_intake_test.cpp
@@ -1,6 +1,6 @@
-#include "gtest/gtest.h"
 #include "ground_gear_intake.h"
 #include "queue_types.h"
+#include "gtest/gtest.h"
 
 
 TEST(GroundGearIntakeTest, CanPickupWithoutCurrentSpike) {
@@ -31,23 +31,6 @@ TEST(GroundGearIntakeTest, CanCarryWithoutCurrentSpike) {
   
   for(int i = 0; i < 1000; i++) {
     input->set_current(20.);
-    output = gear_intake.Update(input);
-    EXPECT_FALSE(output->solenoid());
-    EXPECT_NEAR(output->roller_voltage(), 0., 1e-5);
-  }
-}
-
-TEST(GroundGearIntakeTest, CanPickupWithCurrentSpike) {
- c2017::ground_gear_intake::GroundGearIntake gear_intake;
- c2017::ground_gear_intake::GroundGearIntakeInputProto input;
- c2017::ground_gear_intake::GroundGearIntakeGoalProto goal;
- c2017::ground_gear_intake::GroundGearIntakeOutputProto output;
-
-  goal->set_goal(c2017::ground_gear_intake::GroundGearIntakeGoal::PICKUP);
-  gear_intake.SetGoal(goal);
-  
-  for(int i = 0; i < 1000; i++) {
-    input->set_current(134.);
     output = gear_intake.Update(input);
     EXPECT_FALSE(output->solenoid());
     EXPECT_NEAR(output->roller_voltage(), 0., 1e-5);
@@ -150,6 +133,36 @@ TEST(GroundGearIntakeTest, CanScoreAfterCarrying) {
     output = gear_intake.Update(input);
     EXPECT_FALSE(output->solenoid());
     EXPECT_NEAR(output->roller_voltage(), -12., 1e-5);
+
+  }
+}
+
+
+TEST(GroundGearIntakeTest, CanScoreAfterPickup) {
+ c2017::ground_gear_intake::GroundGearIntake gear_intake;
+ c2017::ground_gear_intake::GroundGearIntakeInputProto input;
+ c2017::ground_gear_intake::GroundGearIntakeGoalProto goal;
+ c2017::ground_gear_intake::GroundGearIntakeOutputProto output;
+
+  goal->set_goal(c2017::ground_gear_intake::GroundGearIntakeGoal::PICKUP);
+  gear_intake.SetGoal(goal);
+  
+  for(int i = 0; i < 100; i++) {
+    input->set_current(20.);
+    output = gear_intake.Update(input);
+    EXPECT_TRUE(output->solenoid());
+    EXPECT_NEAR(output->roller_voltage(), 12., 1e-5);
+  }
+
+  goal->set_goal(c2017::ground_gear_intake::GroundGearIntakeGoal::SCORE);
+  gear_intake.SetGoal(goal);
+  
+  for(int i = 0; i < 1000; i++) {
+    input->set_current(20.);
+    output = gear_intake.Update(input);
+    EXPECT_FALSE(output->solenoid());
+    EXPECT_NEAR(output->roller_voltage(), -12., 1e-5);
+
   }
 }
 
