68afcf0d80b47cfe07a35ea7aecf487bf83236b4
Citrus Circuits <1678programming@gmail.com>
2017-09-08 20:06:53 -0700

    Blue hella changes

--- c2017/lemonscript/lemonscript.cpp
+++ c2017/lemonscript/lemonscript.cpp
@@ -102,7 +102,13 @@ void Lemonscript::UpdateAutoRoutine() {
     }
     std::cout << filename << std::endl;
     delete compiler_;
-    compiler_ = new ::lemonscript::LemonScriptCompiler("c2017/lemonscript/auto/" + filename, state_);
+    try {
+      compiler_ = new ::lemonscript::LemonScriptCompiler("c2017/lemonscript/auto/" + filename, state_);
+    } catch (std::string err) {
+      std::cerr << err << std::endl;
+    } catch (char const* err) {
+      std::cerr << err << std::endl;
+    }
   }
 }
 
