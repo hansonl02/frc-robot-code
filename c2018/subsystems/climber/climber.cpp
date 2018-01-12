3df2c0713dcbbaa80b70750bbf8b4292fb95d300
Hanson <hzuoli@gmail.com>
2018-01-11 20:06:16 -0800

    Start climber logic

--- c2018/subsystems/climber/climber.cpp
+++ c2018/subsystems/climber/climber.cpp
@@ -8,9 +8,21 @@ Climber::Climber() : goal_reader_ { QueueManager<ClimbGoal>::Fetch()->MakeReader
 {}
 
 void Climber::Update() {
+  bool should_climb = false;
   ClimbGoal goal;
+
   if (goal_reader_.ReadLastMessage(&goal)) {
     // use goal here
+    switch (Goal) {
+      case NONE:
+        should_climb = false;
+        Winch::Update(encoder, should_climb, outputs_enabled);
+        Batter::Update(should_release, outputs_enabled);
+      case CLIMBING:
+        should_climb = true;
+        Winch::Update(encoder, should_climb, outputs_enabled);
+        Batter::Update(should_release, outputs_enabled);
+    }
   }
 }
 
