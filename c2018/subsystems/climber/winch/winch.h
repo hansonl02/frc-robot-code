86f4d7af0d867dc0b03f35633a16273d174d89ae
Livy <livytaylor47@gmail.com>
2018-01-13 10:40:51 -0800

    Clang format

--- c2018/subsystems/climber/winch/winch.h
+++ c2018/subsystems/climber/winch/winch.h
@@ -2,9 +2,9 @@
 #define C2018_SUBSYSTEMS_CLIMBER_WINCH_WINCH_H_
 
 #include <cmath>
+#include <iostream>
 #include "c2018/subsystems/climber/climber.pb.h"
 #include "c2018/subsystems/climber/queue_types.h"
-#include <iostream>
 
 namespace c2018 {
 
@@ -12,15 +12,16 @@ namespace climber {
 
 namespace winch {
 
-double kWinchRadius = 3; // WRONG
-double kAmountToClimb = 14; // Inches
+double kWinchRadius = 3;     // WRONG
+double kAmountToClimb = 14;  // Inches
 double kRunningVoltage = 6.0;
 
 class Winch {
  public:
   Winch();
 
-  double Update(c2018::climber::ClimberStatusProto *status, double encoder_i, bool should_climb, bool outputs_enabled);
+  double Update(c2018::climber::ClimberStatusProto *status, double encoder_i, bool should_climb,
+                bool outputs_enabled);
 
   bool is_reset() { return reset_; }
   bool has_climbed() { return has_climbed_; }
@@ -32,10 +33,10 @@ class Winch {
   bool has_climbed_;
 };
 
-} // namespace winch
+}  // namespace winch
 
-} // namespace climber
+}  // namespace climber
 
-} // namespace c2018
+}  // namespace c2018
 
-#endif // C2018_SUBSYSTEMS_CLIMBER_WINCH_WINCH_H_
+#endif  // C2018_SUBSYSTEMS_CLIMBER_WINCH_WINCH_H_
--- c2018/subsystems/climber/winch/winch_test.cpp
+++ c2018/subsystems/climber/winch/winch_test.cpp
@@ -1,54 +1,55 @@
-#include "gtest/gtest.h"
 #include "c2018/subsystems/climber/winch/winch.h"
+#include "gtest/gtest.h"
 
 class WinchTest : public ::testing::Test {
-  public:
-    WinchTest() {}
-    
-    double Update(double encoder, bool outputs_enabled, int num_ticks, bool should_climb) {
-      double output;  
-    
-      for (int i = 0; i < num_ticks; i++) {
-        output = winch_.Update(&status_, encoder, should_climb, outputs_enabled);
-      }
-      return output;
+ public:
+  WinchTest() {}
+
+  double Update(double encoder, bool outputs_enabled, int num_ticks, bool should_climb) {
+    double output;
+
+    for (int i = 0; i < num_ticks; i++) {
+      output = winch_.Update(&status_, encoder, should_climb, outputs_enabled);
     }
-    bool is_reset() { return winch_.is_reset(); } 
-    bool has_climbed() { return winch_.has_climbed(); }
-   c2018::climber::ClimberStatusProto status_;
-  private:
-   c2018::climber::winch::Winch winch_;
+    return output;
+  }
+  bool is_reset() { return winch_.is_reset(); }
+  bool has_climbed() { return winch_.has_climbed(); }
+  c2018::climber::ClimberStatusProto status_;
+
+ private:
+  c2018::climber::winch::Winch winch_;
 };
 
 TEST_F(WinchTest, Idle) {
   double output = Update(0.0, true, 3000, false);
-  
+
   EXPECT_NEAR(output, 0.0, 1e-3);
   EXPECT_EQ(status_->climber_state(), c2018::climber::State::IDLE);
-  EXPECT_FALSE(has_climbed()); 
+  EXPECT_FALSE(has_climbed());
 }
 
 TEST_F(WinchTest, Climb) {
   double output;
-  
+
   for (int i = 0; i < 5000; i++) {
     output = Update(i, true, 1, true);
   }
   EXPECT_EQ(status_->climber_state(), c2018::climber::State::IDLE);
   EXPECT_NEAR(output, 0.0, 1e-3);
-  EXPECT_TRUE(has_climbed()); 
+  EXPECT_TRUE(has_climbed());
 }
 
 TEST_F(WinchTest, ClimbReset) {
   double output;
-  
+
   for (int i = 0; i < 200; i++) {
     output = Update(i, true, 1, true);
   }
 
   EXPECT_EQ(status_->climber_state(), c2018::climber::State::CLIMB);
   EXPECT_FALSE(is_reset());
-  EXPECT_NEAR(output, c2018::climber::winch::kRunningVoltage , 1e-3);
+  EXPECT_NEAR(output, c2018::climber::winch::kRunningVoltage, 1e-3);
   EXPECT_FALSE(has_climbed());
 
   for (int i = 0; i < 2; i++) {
@@ -62,10 +63,10 @@ TEST_F(WinchTest, ClimbReset) {
 
   for (int i = 0; i < 5000; i++) {
     output = Update(i, true, 1, true);
- }
+  }
 
   EXPECT_FALSE(is_reset());
   EXPECT_NEAR(output, 0.0, 1e-3);
   EXPECT_EQ(status_->climber_state(), c2018::climber::State::IDLE);
   EXPECT_TRUE(has_climbed());
-} 
+}
