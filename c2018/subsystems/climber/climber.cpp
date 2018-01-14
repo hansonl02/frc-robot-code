2279cf63c74d0f27eeb4d638cbd3e1ba4c450a7a
Hanson <hzuoli@gmail.com>
2018-01-14 15:30:18 -0800

    Fix climber logic and update tests, added comments
    Read from the input queue next time pls ;)

--- c2018/subsystems/climber/climber.cpp
+++ c2018/subsystems/climber/climber.cpp
@@ -4,10 +4,15 @@ namespace c2018 {
 
 namespace climber {
 
+using muan::queues::QueueManager;
+using muan::wpilib::DriverStationProto;
+
 Climber::Climber()
-    : goal_reader_{muan::queues::QueueManager<ClimberGoalProto>::Fetch()->MakeReader()},
-      input_reader_{muan::queues::QueueManager<ClimberInputProto>::Fetch()->MakeReader()},
-      ds_status_{muan::queues::QueueManager<muan::wpilib::DriverStationProto>::Fetch()->MakeReader()} {}
+    : status_queue_{QueueManager<ClimberStatusProto>::Fetch()},
+      goal_reader_{QueueManager<ClimberGoalProto>::Fetch()->MakeReader()},
+      input_reader_{QueueManager<ClimberInputProto>::Fetch()->MakeReader()},
+      output_queue_{QueueManager<ClimberOutputProto>::Fetch()},
+      ds_status_{QueueManager<DriverStationProto>::Fetch()->MakeReader()} {}
 
 void Climber::Update() {
   bool should_climb = false;
@@ -17,25 +22,22 @@ void Climber::Update() {
 
   ClimberStatusProto status_proto;
   ClimberOutputProto output_proto;
-  ClimberGoalProto goal_proto;
-
-  ClimberGoal goal;
-  ClimberInput input;
+  ClimberGoalProto goal_proto = goal_reader_.ReadLastMessage().value();
+  ClimberInputProto input = input_reader_.ReadLastMessage().value();
 
   if (!goal_reader_.ReadLastMessage()) {
     goal_proto->set_climber_goal(NONE);
   }
-
   if (input_reader_.ReadLastMessage()) {
     outputs_enabled = ds_status_.ReadLastMessage().value()->is_sys_active();
-    // use goal here
-    switch (goal.climber_goal()) {
+    // SETTING STATUS AND SHOULD_CLIMB GOAL
+    switch (goal_proto->climber_goal()) {
       case NONE:
         should_climb = false;
         status_proto->set_climber_state(IDLE);
         break;
       case APPROACHING:
-        goal.set_put_down_batter(true);
+        goal_proto->set_put_down_batter(true);
         status_proto->set_climber_state(APPROACH);
         break;
       case CLIMBING:
@@ -44,16 +46,20 @@ void Climber::Update() {
         break;
     }
 
-    winch_output = winch_.Update(&status_proto, input.position(), should_climb, outputs_enabled);
-    batter_output = batter_.Update(goal.put_down_batter(), outputs_enabled);
+    // UPDATING MECHANISMS
+    winch_output = winch_.Update(input->position(), should_climb, outputs_enabled);
+    batter_output = batter_.Update(goal_proto->put_down_batter(), outputs_enabled);
 
+    // SETTING OUTPUTS
     output_proto->set_release_solenoid(batter_output);
     output_proto->set_voltage(winch_output);
 
+    // SETTING STATUS
     if (winch_.has_climbed()) {
       status_proto->set_climber_state(DONE);
     }
 
+    // WRITING TO QUEUES
     status_queue_->WriteMessage(status_proto);
     output_queue_->WriteMessage(output_proto);
   }
--- c2018/subsystems/climber/climber_test.cpp
+++ c2018/subsystems/climber/climber_test.cpp
@@ -2,19 +2,154 @@
 #include "gtest/gtest.h"
 #include "muan/queues/queue_manager.h"
 
-Climber climber;
+c2018::climber::Climber climber;
 
 TEST(ClimberTest, Idle) {
-  c2018::climber::ClimberGoalQueue climber_goal_queue;
-  c2018::climber::ClimberInputQueue climber_input_queue;
-  muan::wpilib::DriverStationQueue driver_station_queue;
+  // CREATING QUEUES
+  c2018::climber::ClimberGoalQueue* climber_goal_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberGoalProto>::Fetch();
+  c2018::climber::ClimberInputQueue* climber_input_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberInputProto>::Fetch();
+  muan::wpilib::DriverStationQueue* driver_station_queue =
+      muan::queues::QueueManager<muan::wpilib::DriverStationProto>::Fetch();
+  c2018::climber::ClimberStatusQueue::QueueReader climber_status_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberStatusProto>::Fetch()->MakeReader();
+  c2018::climber::ClimberOutputQueue::QueueReader climber_output_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberOutputProto>::Fetch()->MakeReader();
 
+  // CREATING PROTOS
   c2018::climber::ClimberGoalProto climber_goal_proto;
   c2018::climber::ClimberInputProto climber_input_proto;
+  c2018::climber::ClimberStatusProto climber_status_proto;
+  c2018::climber::ClimberOutputProto climber_output_proto;
   muan::wpilib::DriverStationProto driver_station_proto;
 
-  climber_goal_proto.set_put_down_batter(false);
-  climber_goal_proto.set_climber_goal(NONE);  // TODO(Gemma and Hanson and Tim) continue writing tests
+  // SETTING GOALS & INPUTS
+  climber_goal_proto->set_put_down_batter(false);
+  climber_goal_proto->set_climber_goal(c2018::climber::Goal::NONE);
+  driver_station_proto->set_is_sys_active(true);
+  climber_input_proto->set_position(0);
+
+  // WRITING TO QUEUES
+  climber_input_queue->WriteMessage(climber_input_proto);
+  climber_goal_queue->WriteMessage(climber_goal_proto);
+  driver_station_queue->WriteMessage(driver_station_proto);
 
   climber.Update();
+
+  // READING QUEUES
+  climber_output_queue.ReadLastMessage(&climber_output_proto);
+  climber_status_queue.ReadLastMessage(&climber_status_proto);
+
+  EXPECT_EQ(climber_output_proto->voltage(), 0.);
+  EXPECT_FALSE(climber_output_proto->release_solenoid());
+  EXPECT_EQ(climber_status_proto->climber_state(), c2018::climber::State::IDLE);
+  EXPECT_EQ(climber_status_proto->observed_velocity(), 0.);
+  EXPECT_EQ(climber_status_proto->observed_height(), 0.);
+}
+
+TEST(ClimberTest, Approach) {
+  // MAKING QUEUES
+  c2018::climber::ClimberGoalQueue* climber_goal_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberGoalProto>::Fetch();
+  c2018::climber::ClimberInputQueue* climber_input_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberInputProto>::Fetch();
+  muan::wpilib::DriverStationQueue* driver_station_queue =
+      muan::queues::QueueManager<muan::wpilib::DriverStationProto>::Fetch();
+  c2018::climber::ClimberStatusQueue::QueueReader climber_status_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberStatusProto>::Fetch()->MakeReader();
+  c2018::climber::ClimberOutputQueue::QueueReader climber_output_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberOutputProto>::Fetch()->MakeReader();
+
+  // MAKING PROTOS
+  c2018::climber::ClimberGoalProto climber_goal_proto;
+  c2018::climber::ClimberInputProto climber_input_proto;
+  c2018::climber::ClimberStatusProto climber_status_proto;
+  c2018::climber::ClimberOutputProto climber_output_proto;
+  muan::wpilib::DriverStationProto driver_station_proto;
+
+  // SETTING GOALS
+  climber_goal_proto->set_put_down_batter(true);
+  climber_goal_proto->set_climber_goal(c2018::climber::Goal::APPROACHING);
+  driver_station_proto->set_is_sys_active(true);
+
+  // SETTING INPUT
+  climber_input_proto->set_position(0.0);
+
+  // WRITING GOALS AND INPUT
+  climber_input_queue->WriteMessage(climber_input_proto);
+  climber_goal_queue->WriteMessage(climber_goal_proto);
+  driver_station_queue->WriteMessage(driver_station_proto);
+
+  climber.Update();
+
+  // READING QUEUES
+  climber_output_queue.ReadLastMessage(&climber_output_proto);
+  climber_status_queue.ReadLastMessage(&climber_status_proto);
+
+  // TEST VALUES
+  EXPECT_EQ(climber_output_proto->voltage(), 0.);
+  EXPECT_TRUE(climber_output_proto->release_solenoid());
+  EXPECT_EQ(climber_status_proto->climber_state(), c2018::climber::State::APPROACH);
+  EXPECT_EQ(climber_status_proto->observed_velocity(), 0.);
+  EXPECT_EQ(climber_status_proto->observed_height(), 0.);
+}
+
+TEST(ClimberTest, Climb) {
+  // CREATING QUEUES
+  c2018::climber::ClimberGoalQueue* climber_goal_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberGoalProto>::Fetch();
+  c2018::climber::ClimberInputQueue* climber_input_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberInputProto>::Fetch();
+  muan::wpilib::DriverStationQueue* driver_station_queue =
+      muan::queues::QueueManager<muan::wpilib::DriverStationProto>::Fetch();
+  c2018::climber::ClimberStatusQueue::QueueReader climber_status_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberStatusProto>::Fetch()->MakeReader();
+  c2018::climber::ClimberOutputQueue::QueueReader climber_output_queue =
+      muan::queues::QueueManager<c2018::climber::ClimberOutputProto>::Fetch()->MakeReader();
+
+  // CREATING PROTOS
+  c2018::climber::ClimberGoalProto climber_goal_proto;
+  c2018::climber::ClimberInputProto climber_input_proto;
+  c2018::climber::ClimberStatusProto climber_status_proto;
+  c2018::climber::ClimberOutputProto climber_output_proto;
+  muan::wpilib::DriverStationProto driver_station_proto;
+
+  // SETTING GOALS
+  climber_goal_proto->set_put_down_batter(true);
+  climber_goal_proto->set_climber_goal(c2018::climber::Goal::CLIMBING);
+  driver_station_proto->set_is_sys_active(true);
+
+  // WRITING GOAL AND DS STATUS
+  climber_goal_queue->WriteMessage(climber_goal_proto);
+  driver_station_queue->WriteMessage(driver_station_proto);
+
+  // UPDATING CLIMBER
+  for (int i = 0; i < 200; i++) {
+    climber_input_proto->set_position(i);
+    climber_input_queue->WriteMessage(climber_input_proto);
+    climber.Update();
+  }
+
+  // CHECKING HALFWAY THROUGH CLIMB
+  climber_status_queue.ReadLastMessage(&climber_status_proto);
+  EXPECT_EQ(climber_status_proto->climber_state(), c2018::climber::State::CLIMB);
+
+  // UPDATING FOR LONGER
+  for (int i = 0; i < 3000; i++) {
+    climber_input_proto->set_position(i);
+    climber_input_queue->WriteMessage(climber_input_proto);
+    climber.Update();
+  }
+
+  // READING QUEUES
+  climber_output_queue.ReadLastMessage(&climber_output_proto);
+  climber_status_queue.ReadLastMessage(&climber_status_proto);
+
+  // TESTING OUTPUTS
+  EXPECT_EQ(climber_output_proto->voltage(), 0.);
+  EXPECT_TRUE(climber_output_proto->release_solenoid());
+  EXPECT_EQ(climber_status_proto->climber_state(), c2018::climber::State::DONE);
+  EXPECT_EQ(climber_status_proto->observed_velocity(), 0.);
+  EXPECT_EQ(climber_status_proto->observed_height(), 0.);
 }
