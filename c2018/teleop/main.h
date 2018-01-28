d3cafe89306b03fcff485dcd359d6be445dec90d
Hanson <hzuoli@gmail.com>
2018-01-28 11:45:23 -0800

    Add includes, and switch includes from .cpp to .h

--- c2018/teleop/main.h
+++ c2018/teleop/main.h
@@ -4,6 +4,13 @@
 #include <atomic>
 #include "muan/teleop/joystick.h"
 #include "muan/wpilib/ds_sender.h"
+#include "c2018/subsystems/climber/climber.h"
+#include "c2018/subsystems/score_subsystem/score_subsystem.h"
+#include "WPILib.h"
+#include "muan/queues/queue_manager.h"
+#include "muan/wpilib/queue_types.h"
+#include "third_party/aos/common/util/phased_loop.h"
+#include "third_party/frc971/control_loops/drivetrain/queue_types.h"
 
 namespace c2018 {
 namespace teleop {
