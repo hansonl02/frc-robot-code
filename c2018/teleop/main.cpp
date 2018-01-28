57537ff3781cbaf05e93c11eafb39202dda8d52c
Hanson <hzuoli@gmail.com>
2018-01-28 11:02:59 -0800

    Add buttons to main.cpp and add some logic

--- c2018/teleop/main.cpp
+++ c2018/teleop/main.cpp
@@ -21,28 +21,37 @@ TeleopBase::TeleopBase()
       gamepad_{2, QueueManager<JoystickStatusProto>::Fetch("gamepad")},
 
       ds_sender_{QueueManager<DriverStationProto>::Fetch(),
-      QueueManager<GameSpecificStringProto>::Fetch()} {
-        first_level_score_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::A_BUTTON));
-        second_level_score_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::B_BUTTON));
-        third_level_score_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::X_BUTTON));
-        score_height_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::Y_BUTTON));
-        initialize_climb_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::BACK));
-        climb_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::START));
-        godmode_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::LEFT_CLICK_IN));
+                 QueueManager<GameSpecificStringProto>::Fetch()} {
+  first_level_score_ =
+      gamepad_.MakeButton(uint32_t(muan::teleop::XBox::A_BUTTON));
+  second_level_score_ =
+      gamepad_.MakeButton(uint32_t(muan::teleop::XBox::B_BUTTON));
+  third_level_score_ =
+      gamepad_.MakeButton(uint32_t(muan::teleop::XBox::X_BUTTON));
+  score_height_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::Y_BUTTON));
+  initialize_climb_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::BACK));
+  climb_ = gamepad_.MakeButton(uint32_t(muan::teleop::XBox::START));
+  godmode_ = gamepad_.MakeButton(
+      uint32_t(muan::teleop::XBox::LEFT_CLICK_IN));  // TODO(hanson/gemma/ellie)
+                                                     // add godmodes for
+                                                     // intaking/outtaking
 
-        godmode_elevator_down_ = gamepad_.MakeAxis(1, .7);  // Left Joystick South
-        godmode_elevator_up_ = gamepad_.MakeAxis(4, .7)  // TODO(David/Gemma/Anyone with any knowledge) Figure out what buttons correspond to what axis
+  godmode_elevator_down_ = gamepad_.MakeAxis(1, .7);  // Left Joystick South
+  godmode_elevator_up_ = gamepad_.MakeAxis(4, .7)     // TODO(David/Gemma/) Figure
+                                                      // out what buttons
+                                                      // correspond to what axis
 
-        intake_ = gamepad_.MakeAxis(3, .7);  // Right Trigger
-        outtake_ = gamepad_.MakeAxis(2, .7);  // Left Trigger
+  intake_ = gamepad_.MakeAxis(3, .7);  // Right Trigger
+  outtake_ = gamepad_.MakeAxis(2, .7); // Left Trigger
 
-        score_back_ = gamepad_.MakePov(0, muan::teleop::Pov::kSouth);
-        score_front_ = gamepad_.MakePov(0, muan::teleop::Pov::kNorth);
+  score_back_ = gamepad_.MakePov(0, muan::teleop::Pov::kSouth);
+  score_front_ = gamepad_.MakePov(0, muan::teleop::Pov::kNorth);
 
-        shifting_low_ = throttle_.MakeButton(4);
-        shifting_high_ = throttle_.MakeButton(5);
-        quickturn_ = wheel_.MakeButton(5);
-      }
+  shifting_low_ = throttle_.MakeButton(4);
+  shifting_high_ = throttle_.MakeButton(5);
+
+  quickturn_ = wheel_.MakeButton(5);
+}
 
 void TeleopBase::operator()() {
   aos::time::PhasedLoop phased_loop(std::chrono::milliseconds(20));
@@ -124,6 +133,51 @@ void TeleopBase::SendDrivetrainMessage() {
   drivetrain_goal->mutable_teleop_command()->set_quick_turn(quickturn);
 
   QueueManager<DrivetrainGoal>::Fetch()->WriteMessage(drivetrain_goal);
+}
+
+void TeleopBase::SendScoreSubsystemMessage() {
+  //Godmode
+  god_mode_ = god_mode_ != godmode_->was_clicked();
+
+  if (god_mode_ && godmode_elevator_up_->was_clicked()) {
+    //TODO(hanson/gemma/ellie) kyle pls
+  }
+
+  //Toggle elevator heights
+  if (first_level_score_->was_clicked()) {
+    score_subsystem_goal_->set_elevator_height(c2018::score_subsystem::HEIGHT_0);
+  } else if (second_level_score_->was_clicked()) {
+    score_subsystem_goal_->set_elevator_height(c2018::score_subsystem::HEIGHT_1);
+  } else if (third_level_score->was_clicked()) {
+    score_subsystem_goal_->set_elevator_height(c2018::score_subsystem::HEIGHT_2);
+  } else if (score_height_->was_clicked()) {
+    score_subsystem_goal_->set_elevator_height(c2018::score_subsystem::HEIGHT_SCORE);
+  }
+
+  //Intake Modes
+  if (intake_->is_pressed()){
+    score_subsystem_goal_->set_intake_mode(c2018::score_subsystem::INTAKE);
+  }
+  if (outtake_->is_pressed()){
+    score_subsystem_goal_->set_intake_mode(c2018::score_subsystem::OUTTAKE);
+  }
+
+  //Scoring
+  if (score_front_->is_pressed()) {
+    score_subsystem_goal_->set_claw_mode(c2018::score_subsystem::SCORE_F);
+  }
+  if (score_back_->is_pressed()) {
+    score_subsystem_goal_->set_claw_mode(c2018::score_subsystem::SCORE_B);
+  }
+}
+
+void TeleopBase::SendClimbSubsystemMessage() {
+  if (initialize_climb_->was_clicked()) {
+    climber_goal->set_climber_goal(c2018::climber::APPROACHING);
+    if (climb_->was_clicked()) {
+      climber_goal->set_climber_goal(c2018::climber::CLIMBING);
+    }
+  }
 }
 
 }  // namespace teleop
