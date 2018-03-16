bc04968d52924a9c3176831ba1e13739148caaea
Hanson <hzuoli@gmail.com>
2018-03-15 20:01:05 -0700

    Delete unnecessary code in climber

--- c2018/subsystems/climber/climber.cpp
+++ c2018/subsystems/climber/climber.cpp
@@ -60,16 +60,10 @@ void Climber::Update() {
         status->set_climber_state(CLIMB);
         break;
     }
-<<<<<<< HEAD
     // SETTING STATUS
-    if (winch_.has_climbed()) {
-      status->set_climber_state(DONE);
-    }
     if (status->climber_state() == BATTER || status->climber_state() == CLIMB) {
       hook_output_ = false;
     }
-=======
->>>>>>> 618b58a9349fef386977210ecc05b90c417e6c47
   } else {
     status->set_climber_state(IDLE);
   }
