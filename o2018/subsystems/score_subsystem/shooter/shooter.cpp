977329adcfbf69d43e8b8d70f658ab4f35caef90
hansonl02 <hzuoli@gmail.com>
2018-08-01 19:38:06 -0700

    Fixes

---
+++ o2018/subsystems/score_subsystem/shooter/shooter.cpp
@@ -0,0 +1,87 @@
+#include "o2018/subsystems/score_subsystem/shooter.h"
+
+namespace o2018 {
+namespace score_subsystem {
+namespace shooter {
+
+bool Shooter::Update(bool has_cube, bool should_punch, bool outputs_enabled) {
+  // Start of intake
+  bool intake_solenoid_close = false;
+  bool intake_solenoid_open = false;
+
+  if (outputs_enabled) {
+    switch (intake_mode_) {
+      case IntakeGoal::INTAKE:
+        intake_voltage_ = kIntakeVoltage;
+        intake_solenoid_close = false;
+        intake_solenoid_open = false;
+        break;
+      case IntakeGoal::INTAKE_OPEN:
+        intake_voltage_ = kIntakeVoltage;
+        intake_solenoid_close = false;
+        intake_solenoid_open = true;
+        break;
+      case IntakeGoal::INTAKE_CLOSE:
+      case IntakeGoal::SETTLE:
+        intake_voltage_ = kIntakeVoltage;
+        intake_solenoid_close = true;
+        intake_solenoid_open = false;
+        break;
+      case IntakeGoal::OUTTAKE_SLOW:
+        intake_voltage_ = kSlowOuttakeVoltage;
+        intake_solenoid_close = true;
+        intake_solenoid_open = false;
+        break;
+      case IntakeGoal::OUTTAKE_FAST:
+        intake_voltage_ = kFastOuttakeVoltage;
+        intake_solenoid_close = true;
+        intake_solenoid_open = false;
+        break;
+      case IntakeGoal::DROP:
+        intake_voltage_ = 0;
+        intake_solenoid_close = false;
+        intake_solenoid_open = true;
+        break;
+      case IntakeGoal::INTAKE_NONE:
+        if (has_cube) {
+          intake_voltage_ = kHoldingVoltage;
+        } else {
+          intake_voltage_ = 0;
+        }
+        intake_solenoid_close = true;
+        intake_solenoid_open = false;
+        break;
+    }
+  } else {
+    intake_voltage_ = 0;
+  }
+
+  // Logic to make sure it actually has a cube
+  switch (pinch_state_) {
+    case MOVING:
+      if (intake_solenoid_close) {
+        has_cube_for_ticks_--;
+      } else {
+        has_cube_for_ticks_ = kNumHasCubeTicks;
+      }
+      if (has_cube_for_ticks_ < 0) {
+        pinch_state_ = IDLE_WITH_CUBE;
+      }
+      break;
+    case IDLE_WITH_CUBE:
+      if (intake_solenoid_open) {
+        pinch_state_ = IDLE_NO_CUBE;
+      }
+      break;
+    case IDLE_NO_CUBE:
+      if (intake_solenoid_close) {
+        pinch_state_ = MOVING;
+      }
+      break;
+  }
+
+}
+
+} // namespace shooter
+} // namespace score_subsystem
+} // namespace o2018
---
+++ o2018/subsystems/score_subsystem/shooter/shooter.h
@@ -0,0 +1,23 @@
+#ifndef O2018_SUBSYSTEMS_SCORE_SUBSYSTEM_PUNCH_PUNCH_H
+#define O2018_SUBSYSTEMS_SCORE_SUBSYSTEM_PUNCH_PUNCH_H
+
+namespace o2018 {
+namespace score_subsystem {
+namespace shooter {
+
+// Intake Voltage constants
+constexpr double kSlowOuttakeVoltage = -6;
+constexpr double kFastOuttakeVoltage = -9;
+constexpr double kIntakeVoltage = 12;
+
+class Shooter {
+ public:
+  Shooter() = default;
+  bool Update(bool has_cube, bool should_punch, bool outputs_enabled);
+}
+
+} // namespace shooter
+} // namespace score_subsystem
+} // namespace o2018
+
+#endif // O2018_SUBSYSTEMS_SCORE_SUBSYSTEM_PUNCH_PUNCH_H
